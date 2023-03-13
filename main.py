6f6a47342d77ade534d14b06887364df1d00b960
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-03-13 15:29:59 +0100

    On s'occupe de l'interface graphique et on ajoute de la sécurité dans le hashage et on affiche les différentes erreurs
    On affiche les erreurs en fonction du mot de passe qui a été entré

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from tkinter import messagebox
 import re
 import hashlib
 
+
 class PasswordValidatorGUI:
     def __init__(self, master):
         self.master = master
         self.master.title("Validation et cryptage de mot de passe")
-        self.master.geometry("400x200")  # Agrandir la fenêtre à 400x200 pixels
+        self.master.geometry("400x200")
 
-        # Centre la fenêtre sur l'écran
         screen_width = self.master.winfo_screenwidth()
         screen_height = self.master.winfo_screenheight()
         x_cordinate = int((screen_width / 2) - (400 / 2))
@@ -20,20 +20,17 @@ class PasswordValidatorGUI:
         self.validation_frame = tk.Frame(self.master, padx=10, pady=10)
         self.validation_frame.pack()
 
-        # Ajouter l'étiquette "Password" en haut de la zone de texte
         self.password_label = tk.Label(self.validation_frame, text="Password", fg="indigo", font=("Arial", 14))
         self.password_label.grid(row=0, column=0, pady=5)
 
-        # Centrer la zone de texte
         self.password_entry = ttk.Entry(self.validation_frame, width=20, show="*")
         self.password_entry.grid(row=1, column=0, pady=5)
         self.password_entry.focus()
-        self.validation_frame.columnconfigure(0, weight=1)  # Centrer la colonne
+        self.validation_frame.columnconfigure(0, weight=1)
 
-        # Centrer les boutons
         self.button_frame = tk.Frame(self.master, padx=10, pady=10)
         self.button_frame.pack()
-        self.button_frame.columnconfigure(0, weight=1)  # Centrer la colonne
+        self.button_frame.columnconfigure(0, weight=1)
 
         self.validate_button = ttk.Button(self.button_frame, text="Valider", command=self.validate_password)
         self.validate_button.grid(row=0, column=0, padx=5)
@@ -47,34 +44,38 @@ class PasswordValidatorGUI:
 
     def validate_password(self):
         password = self.password_entry.get()
-        if len(password) == 0:
-            messagebox.showerror("Erreur", "Veuillez entrer un mot de passe.")
-            return
+        errors = self.validate_password_rules(password)  # Valide le mot de passe selon les règles
+        if errors:  # Si des erreurs sont retournées, affiche un message d'erreur
+            error_message = "Le mot de passe n'est pas valide : \n\n{}".format("\n".join(errors))
+            messagebox.showerror("Erreur", error_message)
+        else:  # Si le mot de passe est valide, active le bouton "Crypter"
+            messagebox.showinfo("Succès", "Le mot de passe est valide.")
+            self.encrypt_button.config(state=tk.NORMAL)
 
-        while True:
-            if len(password) < 8:
-                messagebox.showerror("Erreur", "Le mot de passe doit contenir au moins 8 caractères.")
-            elif not re.search("[a-z]", password):
-                messagebox.showerror("Erreur", "Le mot de passe doit contenir au moins une lettre minuscule.")
-            elif not re.search("[A-Z]", password):
-                messagebox.showerror("Erreur", "Le mot de passe doit contenir au moins une lettre majuscule.")
-            elif not re.search("[0-9]", password):
-                messagebox.showerror("Erreur", "Le mot de passe doit contenir au moins un chiffre.")
-            elif not re.search("[!@#$%^&*()_+-={};':\"|,.<>?~`]", password):
-                messagebox.showerror("Erreur", "Le mot de passe doit contenir au moins un caractère spécial.")
-            else:
-                messagebox.showinfo("Succès", "Le mot de passe est valide.")
-                self.encrypt_button.config(state=tk.NORMAL)
-                break
-            password = tk.simpledialog.askstring("Nouveau mot de passe", "Veuillez entrer un nouveau mot de passe : ", show='*')
-            if password is None:
-                break
+    def validate_password_rules(self, password):
+        errors = []
+        if len(password) < 8:
+            errors.append("Le mot de passe doit contenir au moins 8 caractères.")
+        if not re.search("[a-z]", password):
+            errors.append("Le mot de passe doit contenir au moins une lettre minuscule.")
+        if not re.search("[A-Z]", password):
+            errors.append("Le mot de passe doit contenir au moins une lettre majuscule.")
+        if not re.search("[0-9]", password):
+            errors.append("Le mot de passe doit contenir au moins un chiffre.")
+        if not re.search("[!@#$%^&*()_+-={};':\"|,.<>?~`]", password):
+            errors.append("Le mot de passe doit contenir au moins un caractère spécial.")
+        return errors
 
     def encrypt_password(self):
+        password = self.password_entry
         password = self.password_entry.get()
-        hashed_password = hashlib.sha256(password.encode())
-        encrypted_password = hashed_password.hexdigest()
+        salt = "mysalt"  # Sel fixe utilisé pour saler le mot de passe
+        salted_password = password + salt  # Ajoute le sel au mot de passe
+        hashed_password = hashlib.sha256(salted_password.encode())  # Génère le hash SHA-256 du mot de passe salé
+        encrypted_password = hashed_password.hexdigest()  # Convertit le hash en chaîne hexadécimale
         messagebox.showinfo("Succès", "Le mot de passe a été crypté avec succès : " + encrypted_password)
+        # le salage consiste à ajouter une valeur aléatoire au mot de passe avant de le hacher, ce qui rend plus
+        # difficile la tâche des attaquants qui cherchent à décrypter les mots de passe.
 
 if __name__ == '__main__':
     root = tk.Tk()
