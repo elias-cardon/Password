3326c721fb539e462f63144d7df075b95a861d5d
Elias Cardon <62133614+elias-cardon@users.noreply.github.com>
2023-03-13 13:31:24 +0100

    On s'occupe de l'interface graphique

--- main.py
+++ main.py
@@ -1,34 +1,49 @@
-import hashlib
-import re
 import tkinter as tk
+from tkinter import ttk
 from tkinter import messagebox
+import re
+import hashlib
 
 class PasswordValidatorGUI:
     def __init__(self, master):
         self.master = master
         self.master.title("Validation et cryptage de mot de passe")
+        self.master.geometry("400x200")  # Agrandir la fenêtre à 400x200 pixels
+
+        # Centre la fenêtre sur l'écran
+        screen_width = self.master.winfo_screenwidth()
+        screen_height = self.master.winfo_screenheight()
+        x_cordinate = int((screen_width / 2) - (400 / 2))
+        y_cordinate = int((screen_height / 2) - (200 / 2))
+        self.master.geometry("{}x{}+{}+{}".format(400, 200, x_cordinate, y_cordinate))
 
         self.validation_frame = tk.Frame(self.master, padx=10, pady=10)
         self.validation_frame.pack()
 
-        self.password_label = tk.Label(self.validation_frame, text="Mot de passe : ")
-        self.password_label.grid(row=0, column=0, sticky='w')
+        # Ajouter l'étiquette "Password" en haut de la zone de texte
+        self.password_label = tk.Label(self.validation_frame, text="Password", fg="indigo", font=("Arial", 14))
+        self.password_label.grid(row=0, column=0, pady=5)
 
-        self.password_entry = tk.Entry(self.validation_frame, show="*")
-        self.password_entry.grid(row=0, column=1)
+        # Centrer la zone de texte
+        self.password_entry = ttk.Entry(self.validation_frame, width=20, show="*")
+        self.password_entry.grid(row=1, column=0, pady=5)
+        self.password_entry.focus()
+        self.validation_frame.columnconfigure(0, weight=1)  # Centrer la colonne
 
-        self.validate_button = tk.Button(self.validation_frame, text="Valider", command=self.validate_password)
-        self.validate_button.grid(row=1, column=0, pady=10)
+        # Centrer les boutons
+        self.button_frame = tk.Frame(self.master, padx=10, pady=10)
+        self.button_frame.pack()
+        self.button_frame.columnconfigure(0, weight=1)  # Centrer la colonne
 
-        self.encryption_frame = tk.Frame(self.master, padx=10, pady=10)
-        self.encryption_frame.pack()
+        self.validate_button = ttk.Button(self.button_frame, text="Valider", command=self.validate_password)
+        self.validate_button.grid(row=0, column=0, padx=5)
 
-        self.encrypt_button = tk.Button(self.encryption_frame, text="Crypter", command=self.encrypt_password,
-                                        state=tk.DISABLED)
-        self.encrypt_button.grid(row=1, column=0, pady=10)
+        self.encrypt_button = ttk.Button(self.button_frame, text="Crypter", command=self.encrypt_password,
+                                         state=tk.DISABLED)
+        self.encrypt_button.grid(row=0, column=1, padx=5)
 
         self.message_label = tk.Label(self.master, text="")
-        self.message_label.pack()
+        self.message_label.pack(pady=10)
 
     def validate_password(self):
         password = self.password_entry.get()
